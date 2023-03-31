b2048edb6e5f0867724069aa234de4b949307155
Misbahudin <mizzc0d3@gmail.com>
2023-03-31 18:01:05 +0700

    remove script datepicker duplicate

--- index.php
+++ index.php
@@ -141,14 +141,6 @@ $smartphones = getAllSmartphone();
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
     </script>
-
-    <script>
-        $(function() {
-            $('#datepicker').datepicker();
-        });
-    </script>
-
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js">
